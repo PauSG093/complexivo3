44e11bf1026a9a73437634e6c10c75e3eb9e3ac2
PauSG093 <paulinasacoto@gmail.com>
2026-04-15 11:47:58 -0500

    1era

--- components/Tarjeta.tsx
+++ components/Tarjeta.tsx
@@ -1,14 +1,16 @@
-import { Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, { useState } from 'react';
-import { Button } from 'react-native/types_generated/index';
+import { FlatList, Switch } from 'react-native-gesture-handler';
 
 export default function Tarjeta({datos}: any) {
 
-    const[ocultar, setocultar] =useState(second)
+    const[ocultar, setocultar] =useState(false)
+    const [positivo, setpositivo] = useState(false)
     //console.log(datos.titulo);
 
     return (
-        <TouchableOpacity style= {styles.container}
+        <TouchableOpacity 
+            style= {styles.container}
             onPress={()=> setocultar(true)}
             >
             <Text>Pelicula: {datos.tittulo}</Text>
@@ -24,8 +26,28 @@ export default function Tarjeta({datos}: any) {
                 source={{uri: datos.imagen}}
                 style={styles.img}
                 /> 
-                
 
+                <Text>Comentarios {"/n/n/n/n"}</Text>
+
+                <View style={{flexDirection:'row'}}>
+                    <Text>Positivos</Text>
+                    <Switch 
+                        value={positivo} 
+                        onChange={()=> setpositivo(!positivo)}
+                    />
+                
+                    <Text>Negativos</Text>
+                </View>
+                {
+                    positivo == true
+                    ?<Text>Comentarios positivo</Text>
+                    : <FlatList
+                        data={datos.opiniones.opiniones_negativas.detalles}
+                        renderItem={({item})=>   
+                            <Text>{item.opinion}</Text>
+                        }
+                    />                     
+                }
 
                  <Button title='cerrar' onPress={()=> setocultar(false)}/>
             </Modal>
@@ -45,5 +67,5 @@ const styles = StyleSheet.create({
         height: 250,
         resizeMode: 'contain'
     }
-});
+})
     
